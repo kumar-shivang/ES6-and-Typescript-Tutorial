491ac5141db1e578a3dc17c523c181f466c38b4e
kumar-shivang <shivangkumar1857@gmail.com>
2024-05-06 19:40:20 +0530

    symbol iterator

---
+++ symbol_iterator.ts
@@ -0,0 +1,11 @@
+let str: string = "Hello World";
+let arr: number[] = [1, 2, 3];
+let num: number = 111;
+let obj = { name: "Dibbu" };
+
+console.log(`for string ${typeof str[Symbol.iterator]}`);
+console.log(`for array ${typeof arr[Symbol.iterator]}`);
+// console.log(`for nummber ${typeof num[Symbol.iterator]}`);
+// console.log(`for object ${typeof obj[Symbol.iterator]}`);
+
+console.log("It is used to find if an object is iterable or not.");
